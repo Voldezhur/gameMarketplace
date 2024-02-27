f91433cbcd97b6bde67140d4ebe8bb578638f748
Voldezhur <voldezhur@gmail.com>
2024-02-27 10:39:26 +0300

    Reverted to original

--- config/db_params.php
+++ config/db_params.php
@@ -2,8 +2,8 @@
 
 // Массив с параметрами подключения к базе данных
 return array(
-    'host' => 'localhost',
-    'dbname' => 'burnfeniks_kolosok',
-    'user' => '046351469_test2',
-    'password' => '046351469_test2',
+    'host' => 'localhost',  // Voldezhur
+    'dbname' => 'burnfeniks_kolosok',  // games
+    'user' => '046351469_test2',  // root@localhost
+    'password' => '046351469_test2',  // 2911
 );
